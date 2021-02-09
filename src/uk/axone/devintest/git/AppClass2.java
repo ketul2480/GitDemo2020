83e27c9b6f130e95604f5efc4d180beb14614199
ketul2480 <ketul_2480@yahoo.com>
2021-02-09 12:45:51 +0000

    Added new variable

--- src/uk/axone/devintest/git/AppClass2.java
+++ src/uk/axone/devintest/git/AppClass2.java
@@ -1,10 +1,15 @@
 package uk.axone.devintest.git;
 
+
+
 public class AppClass2 {
 
+
+    int i;
+    int x;
     public void doSomethingElse() {
 
         System.out.println("doing something else");
     }
 
-    }
+}
