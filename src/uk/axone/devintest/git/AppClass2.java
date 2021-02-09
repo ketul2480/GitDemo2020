e1379d8413cecb1fd16bdb475db6d845968385a7
ketul2480 <ketul_2480@yahoo.com>
2021-02-09 12:10:40 +0000

    Added new method2 to class2

--- src/uk/axone/devintest/git/AppClass2.java
+++ src/uk/axone/devintest/git/AppClass2.java
@@ -2,12 +2,6 @@ package uk.axone.devintest.git;
 
 public class AppClass2 {
 
-    public void doSomething(){
-
-        System.out.println("doing something");
-
-    }
-
     public void doSomethingElse(){
 
         System.out.println("doing something else");
